1ff05036b47cb2b53cf8067565ea74d2def6e96d
aguroshou <aguroshou0413@gmail.com>
2020-11-26 21:58:11 +0900

    mixamoのアニメーション追加とアニメーション速度変更機能を追加

--- Assets/Scripts/TransformController.cs
+++ Assets/Scripts/TransformController.cs
@@ -1,17 +1,30 @@
 ﻿using System;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.Assertions.Comparers;
 using UnityEngine.UI;
+using System.Globalization;
 
 namespace KiliWare.SampleVRMApp
 {
     public class TransformController : MonoBehaviour
     {
+        private int openAnimationKey = Animator.StringToHash("Entry");
+
         static int VrmControllingObjectNumber;
         [SerializeField] int vrmObjectNumber;
         private Animation animation;
+
         private Animator animator;
+
+        //[SerializeField] AnimatorStateInfo animatorStateInfo;
+        [SerializeField] private float animationSpeed = 0.004f;
+        [SerializeField] private float animationSpeedRate = 1.0f;
+        [SerializeField] private GameObject animationSpeedRateTextGameObject;
+        [SerializeField] private TMP_Text animationSpeedRateText;
+
         private GameObject animatorControllerDropdownGameObject;
         private TMP_Dropdown animatorControllerDropdown;
         private int previousAnimatorControllerDrowdownValue;
@@ -25,86 +38,128 @@ namespace KiliWare.SampleVRMApp
             vrmObjectNumber = VrmControllingObjectNumber;
             animation = GetComponent<Animation>();
             animator = GetComponent<Animator>();
+            animator.runtimeAnimatorController =
+                (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    Resources.Load("AnimatorControllers/MixamoAnimatorController"));
+            animator.updateMode = AnimatorUpdateMode.UnscaledTime;
             animation.playAutomatically = true;
-            transform.Rotate(0f,180f,0f);
+            transform.Rotate(0f, 180f, 0f);
             animatorControllerDropdownGameObject = GameObject.Find("AnimatorControllerDropdown");
             animatorControllerDropdown = animatorControllerDropdownGameObject.GetComponent<TMP_Dropdown>();
             previousAnimatorControllerDrowdownValue = animatorControllerDropdown.value;
             ChangedAnimatorControllerDropdownValue();
+            animationSpeedRateTextGameObject = GameObject.Find("AnimationSpeedRateText");
+            animationSpeedRateText = animationSpeedRateTextGameObject.GetComponent<TextMeshProUGUI>();
+
+            //ヨーロッパ圏での小数点入力の際に必要であるようです。
+            System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("en-us");
         }
+
+        public void SetAnimationFrame(int i_frame)
+        {
+            var clipInfoList = animator.GetCurrentAnimatorClipInfo(0);
+            var clip = clipInfoList[0].clip;
+
+            float time = (float) i_frame / clip.frameRate;
+
+            var stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            var animationHash = stateInfo.shortNameHash;
+
+            animator.Play(animationHash, 0, time);
+        }
+
+        private float currentAnimationTime = 0;
 
         void Update()
         {
+            currentAnimationTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            currentAnimationTime += animationSpeed * animationSpeedRate;
+            animator.Play(openAnimationKey, 0, currentAnimationTime);
+
             if (VrmControllingObjectNumber != vrmObjectNumber)
             {
                 return;
             }
+
+            string tmpAnimationSpeedRateString = animationSpeedRateText.text;
             
+            //tmpAnimationSpeedRateStringの終端文字にゴミが入っているので取り除いています
+            tmpAnimationSpeedRateString =
+                tmpAnimationSpeedRateString.Substring(0, tmpAnimationSpeedRateString.Length - 1);
+            if (float.TryParse(tmpAnimationSpeedRateString, out animationSpeedRate))
+            {
+                Mathf.Clamp(animationSpeedRate, -10f, 10f);
+            }
+            else
+            {
+                animationSpeedRate = 1.0f;
+            }
+
             if (previousAnimatorControllerDrowdownValue != animatorControllerDropdown.value)
             {
                 previousAnimatorControllerDrowdownValue = animatorControllerDropdown.value;
                 ChangedAnimatorControllerDropdownValue();
             }
 
-            // if (Input.GetKey(KeyCode.Mouse0))
-            // {
-            //     Rotate(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
-            // }
-            // if (Input.GetKey(KeyCode.Mouse1))
-            // {
-            //     Move(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
-            // }
-
             if (Input.GetKey(KeyCode.U))
             {
-                transform.Rotate(new Vector3(0f, 2f,0f));
-            }
-            if (Input.GetKey(KeyCode.O))
-            {
-                transform.Rotate(new Vector3(0f, -2f,0f));
-            }
-            if (Input.GetKey(KeyCode.I))
-            {
-                transform.Rotate(new Vector3(2f, 0f,0f));
-            }
-            if (Input.GetKey(KeyCode.K))
-            {
-                transform.Rotate(new Vector3(-2f, 0f,0f));
-            }
-            if (Input.GetKey(KeyCode.J))
-            {
-                transform.Rotate(new Vector3(0f, 0f,-2f));
-            }
-            if (Input.GetKey(KeyCode.L))
-            {
-                transform.Rotate(new Vector3(0f, 0f,2f));
-            }
-            
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Translate(new Vector3(-0.01f, 0f,0f), Space.World);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(new Vector3(0.01f, 0f,0f), Space.World);
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                transform.Translate(new Vector3(0f, 0.01f,0f), Space.World);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(new Vector3(0f, -0.01f,0f), Space.World);
-            }
-            if (Input.GetKey(KeyCode.Q))
-            {
-                transform.Translate(new Vector3(0f, 0,-0.01f), Space.World);
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                transform.Translate(new Vector3(0f, 0,0.01f), Space.World);
+                transform.Rotate(new Vector3(0f, 2f, 0f));
             }
 
+            if (Input.GetKey(KeyCode.O))
+            {
+                transform.Rotate(new Vector3(0f, -2f, 0f));
+            }
+
+            if (Input.GetKey(KeyCode.I))
+            {
+                transform.Rotate(new Vector3(2f, 0f, 0f));
+            }
+
+            if (Input.GetKey(KeyCode.K))
+            {
+                transform.Rotate(new Vector3(-2f, 0f, 0f));
+            }
+
+            if (Input.GetKey(KeyCode.J))
+            {
+                transform.Rotate(new Vector3(0f, 0f, -2f));
+            }
+
+            if (Input.GetKey(KeyCode.L))
+            {
+                transform.Rotate(new Vector3(0f, 0f, 2f));
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(-0.01f, 0f, 0f), Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(0.01f, 0f, 0f), Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0f, 0.01f, 0f), Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0f, -0.01f, 0f), Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Translate(new Vector3(0f, 0, -0.01f), Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.Translate(new Vector3(0f, 0, 0.01f), Space.World);
+            }
         }
 
         private void ChangedAnimatorControllerDropdownValue()
@@ -112,46 +167,115 @@ namespace KiliWare.SampleVRMApp
             switch (animatorControllerDropdown.value)
             {
                 case 0:
-                    animator.runtimeAnimatorController =  null;
+                    openAnimationKey = Animator.StringToHash("T-Pose");
+                    //animator.runtimeAnimatorController =  null;
                     break;
                 case 1:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerArmStretching"));
+                    openAnimationKey = Animator.StringToHash("Bboy Hip Hop Move");
+                    //animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerArmStretching"));
                     break;
                 case 2:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerCards"));
+                    openAnimationKey = Animator.StringToHash("Bicycle Crunch");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerCards"));
                     break;
                 case 3:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerDancing"));
+                    openAnimationKey = Animator.StringToHash("Boxing");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerDancing"));
                     break;
                 case 4:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerHapplyIdle"));
+                    openAnimationKey = Animator.StringToHash("Cheering");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerHapplyIdle"));
                     break;
                 case 5:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerOrcIdle"));
+                    openAnimationKey = Animator.StringToHash("Clapping");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerOrcIdle"));
                     break;
                 case 6:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerSitting"));
+                    openAnimationKey = Animator.StringToHash("Cross Jumps");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerSitting"));
                     break;
                 case 7:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerSittingClap"));
+                    openAnimationKey = Animator.StringToHash("Female Walk");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerSittingClap"));
                     break;
                 case 8:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerSittingThumbsUp"));
+                    openAnimationKey = Animator.StringToHash("Flair");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerSittingThumbsUp"));
                     break;
                 case 9:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerSittingYell"));
+                    openAnimationKey = Animator.StringToHash("Floating");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerSittingYell"));
                     break;
                 case 10:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerStandingWBriefcaseIdle"));
+                    openAnimationKey = Animator.StringToHash("Hanging Idle");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerStandingWBriefcaseIdle"));
                     break;
                 case 11:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerTaunt"));
+                    openAnimationKey = Animator.StringToHash("Hip Hop Dancing");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerTaunt"));
                     break;
                 case 12:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerTextingWhileStanding"));
+                    openAnimationKey = Animator.StringToHash("Jogging");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerTextingWhileStanding"));
                     break;
                 case 13:
-                    animator.runtimeAnimatorController =  (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load ("AnimatorControllers/AnimatorControllerThankful"));
+                    openAnimationKey = Animator.StringToHash("Sword And Shield Power Up");
+                    //openAnimationKey = Animator.StringToHash("");
+                    // animator.runtimeAnimatorController =
+                    //     (RuntimeAnimatorController) RuntimeAnimatorController.Instantiate(
+                    //         Resources.Load("AnimatorControllers/AnimatorControllerThankful"));
+                    break;
+                case 14:
+                    openAnimationKey = Animator.StringToHash("Jump");
+                    break;
+                case 15:
+                    openAnimationKey = Animator.StringToHash("Jumping Jacks");
+                    break;
+                case 16:
+                    openAnimationKey = Animator.StringToHash("Martelo 2");
+                    break;
+                case 17:
+                    openAnimationKey = Animator.StringToHash("Sad Walk");
+                    break;
+                case 18:
+                    openAnimationKey = Animator.StringToHash("Situps");
+                    break;
+                case 19:
+                    openAnimationKey = Animator.StringToHash("Skinning Test");
+                    break;
+                case 20:
+                    openAnimationKey = Animator.StringToHash("Spin In Place");
+                    break;
+                case 21:
+                    openAnimationKey = Animator.StringToHash("Standing Greeting");
+                    break;
+                case 22:
+                    openAnimationKey = Animator.StringToHash("Strong Gesture");
+                    break;
+                case 23:
+                    openAnimationKey = Animator.StringToHash("Swimming");
                     break;
                 default:
                     break;

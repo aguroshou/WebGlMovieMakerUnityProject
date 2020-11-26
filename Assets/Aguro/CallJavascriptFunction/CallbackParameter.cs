fdd62f8db1c0646dcc842fd387d2e3bf60f70d52
aguroshou <aguroshou0413@gmail.com>
2020-11-26 13:04:07 +0900

    JavaScriptのhogeMethod関数をUnityから呼べる状態です。

---
+++ Assets/Aguro/CallJavascriptFunction/CallbackParameter.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+ using UnityEngine;
+ using System.Collections;
+ [Serializable] public class CallbackParameter : MonoBehaviour
+ {
+     public string callbackGameObjectName;
+     public string callbackFunctionName;
+
+     void Start()
+     {
+         var executer = new NativeExecuter();
+         var callbackParameter = new CallbackParameter
+         {
+             callbackGameObjectName = gameObject.name,
+             callbackFunctionName = "Callback"
+         };
+         var parameterJson = JsonUtility.ToJson(callbackParameter);
+         executer.Execute("hogeMethod", parameterJson);
+     }
+
+     public void Callback()
+     {
+         Debug.Log("callback from js");
+     }
+ }

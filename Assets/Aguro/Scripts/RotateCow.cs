d4995895672b90b2af7bb53e042d426f4022a1af
aguroshou <aguroshou0413@gmail.com>
2021-01-03 22:12:01 +0900

    WebGLで日本語入力を修正する途中です。

---
+++ Assets/Aguro/Scripts/RotateCow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private float rotationX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationX += -2.4f;
+        transform.rotation = Quaternion.Euler(rotationX, 0.0f, 0.0f);
+    }
+}
